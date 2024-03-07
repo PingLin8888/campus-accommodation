b406d60bfc1f2e71e09ccc9b1f05fe9e0dae02f1
Ping Lin <limphoebe8@gmail.com>
2024-03-07 13:52:33 +0000

    Implementing UserDetail.

---
+++ campus-accommodation-backend/src/main/java/com/phoebe/campusAccommodation/security/user/UserDetails.java
@@ -0,0 +1,70 @@
+package com.phoebe.campusAccommodation.security.user;
+
+import com.phoebe.campusAccommodation.model.Role;
+import com.phoebe.campusAccommodation.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
+
+    private Long id;
+    private String email;
+    private String password;
+
+    private Collection<GrantedAuthority> authorities;
+
+    public static UserDetails buildUserDetails(User user) {
+        List<GrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+        return new UserDetails(user.getId(), user.getEmail(), user.getPassword(), authorities);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
